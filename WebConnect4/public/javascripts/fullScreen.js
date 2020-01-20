e61afeb85657b28bc5e7401fda27a79872aa561c
Matteo Meluzzi <matteo@meluzzi.com>
2020-01-20 20:48:11 +0100

    very last one

--- WebConnect4/public/javascripts/fullScreen.js
+++ WebConnect4/public/javascripts/fullScreen.js
@@ -3,26 +3,34 @@ var elem = document.documentElement;
 
 /* View in fullscreen */
 function openFullscreen() {
-if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-} else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-} else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
+    let elements = document.getElementsByClassName("fullscreen-btn");
+    elements[0].innerHTML = "Close Fullscreen";
+    elements[0].onclick = closeFullscreen;
+    
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
     }
 }
 
 /* Close fullscreen */
 function closeFullscreen() {
-if (document.exitFullscreen) {
-    document.exitFullscreen();
-} else if (document.mozCancelFullScreen) { /* Firefox */
-    document.mozCancelFullScreen();
-} else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-    document.webkitExitFullscreen();
-} else if (document.msExitFullscreen) { /* IE/Edge */
-    document.msExitFullscreen();
-    }
+    let elements = document.getElementsByClassName("fullscreen-btn");
+    elements[0].innerHTML = "Open Fullscreen";
+    elements[0].onclick = openFullscreen;
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+        }
 }
